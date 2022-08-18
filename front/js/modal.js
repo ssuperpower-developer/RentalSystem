4d10205b397e9e42cbd8f4d2cd5f1899db7354ae
SeonHwan-Kim <rlatjsghks4647@naver.com>
2022-08-19 04:40:52 +0900

    [Feat] 날짜선택, Notice

--- front/js/modal.js
+++ front/js/modal.js
@@ -55,11 +55,11 @@ window.onload = function () {
   setInterval(getClock, 1000);
 
   var serverData = [
-    { no: 1, goods: "우산", number: "5" },
-    { no: 2, goods: "양말", number: "2" },
-    { no: 3, goods: "신발", number: "2" },
-    // {no:4, goods:'드라이기', number: '1'},
-    // {no:5, goods:'고데기', number: '2'}
+    { no: 1, goods: "우산", quantity: "5" },
+    { no: 2, goods: "양말", quantity: "2" },
+    { no: 3, goods: "신발", quantity: "2" },
+    // {no:4, goods:'드라이기', quantity: '1'},
+    // {no:5, goods:'고데기', quantity: '2'}
   ];
 
   // 외부데이터 받아와 리스트 생성
@@ -68,7 +68,7 @@ window.onload = function () {
     tr += "<tr>";
     tr += "  <td>" + row.no + "</td>";
     tr += '  <td id="rowList">' + row.goods + "</td>";
-    tr += '  <td id="rowList">' + row.number + "</td>";
+    tr += '  <td id="rowList">' + row.quantity + "</td>";
     tr +=
       '  <td id="rowList">' +
       `<button type="button" id="reserveBtn${row.no}" class="btn" style="background-color: #68CDC1;"data-bs-toggle="modal" data-bs-target="#reserveModal${row.no}">예약하기</button>` +
@@ -104,9 +104,10 @@ window.onload = function () {
           ></button>
         </div>
         <div class="modal-body">
-          1. 기간 내 반납하세요<br />
-          2. 안하면 불이익 있습니다<br />
-          3. 소중히 다뤄주세요<br />
+          1. 연체 시 1일당 1천 원 연체료가 부과됩니다<br />
+          2. 대학원생은 대여가 불가능 합니다<br />
+          3. 공지를 읽지 않아 생기는 불이익은 책임지지 않습니다<br />
+          4. 문의 사항이나 신고접수는 아래 Notice 창을 이용해주세요<br />
           <br />
           Do You Agree?
         </div>
@@ -133,26 +134,88 @@ window.onload = function () {
   </div>
 
   <div class="modal fade" id="chooseReserveDate${row.no}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="ModalLabel">Choose the Date</h5>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                  빌릴 날짜 : <input type="text" id="borrowDate${row.no}" placeholder="날짜 선택"> <br>
-                  ~ <br>
-                  반납할 날짜 : <input type="text" id="returnDate${row.no}" placeholder="날짜 선택"> <br>
-                  <br>
-                  <p>수량 선택: <input id="chooseQuantity${row.no}" type="text" placeholder="수량"></p>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" id="confirmationDate${row.no}" class="btn" style="background-color: #68CDC1;"data-bs-dismiss="modal">reserve</button>
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">NO</button>
-                </div>
-              </div>
-           </div>
-          </div>`;
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="ModalLabel">Choose the Date</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div id="selectDate">
+            빌릴 날짜 : <input type="text" id="borrowDate${row.no}" placeholder="빌릴 날짜">
+            ~ 
+            <input type="text" id="returnDate${row.no}" placeholder="반납 날짜"> <br>
+            <br>
+          </div>
+          <div id="selectQuantity">
+            <p>수량 선택: 
+            <select id="quantity${row.no}">
+            </select>
+          </div>
+        </div>
+        <div class="modal-footer">
+        <button
+          type="button"
+          class="btn"
+          style="background-color: #68cdc1"
+          data-bs-toggle="modal"
+          data-bs-target="#reserveNotice${row.no}"
+        >
+          YES
+        </button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">NO</button>
+        </div>
+      </div>
+    </div>
+</div>
+<div
+    class="modal fade"
+    id="reserveNotice${row.no}"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="ModalLabel" style="color: #FF000F"><strong>NOTICE</strong></h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <span id="returnDateModal${row.no}"></span> <br />
+          2. 대여 시 학생증을 맡겨주셔야 합니다.<br />
+          3. 보증금 1만 원이 필요합니다.(이거는 물품마다 다름)<br />
+          4. 연장은 반납예정일 이전에만 신청이 가능합니다.<br />
+          <br />
+          Do You Agree?
+        </div>
+        <div class="modal-footer">
+          <button
+            id="confirmationDate${row.no}"
+            type="button"
+            class="btn"
+            style="background-color: #68cdc1"
+            data-bs-toggle="modal"
+          >
+            Reserve
+          </button>
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            NO
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+`;
   }
 
   function returnScript(row) {
@@ -185,8 +248,16 @@ window.onload = function () {
           $("#returnDate${row.no}").datepicker({
             onClose: function(selectedDate){
               $("#borrowDate${row.no}").datepicker("option", "maxDate", selectedDate);
+              let selected = $(this).val();
+              let month = selected.substr(5, 2);
+              let day = selected.substr(8, 2);
+              document.getElementById("returnDateModal${row.no}").innerHTML = "1. " + month + "월" + day + "일 까지 반납해주셔야 합니다."
             }
           })
+
+          for(let i = 1; i <= ${row.quantity}; i++){
+            $("#quantity${row.no}").append('<option value="${row.no}">' + i + "개" + '</option>')
+          }
     `;
   }
 
